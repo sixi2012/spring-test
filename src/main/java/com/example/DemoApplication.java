62b7f3586b11b343c70b02a109e48f4d3230d8be
罗绍斌 <shaobin@iMac-luo.local>
2024-04-10 15:00:50 +0800

    add inteceptor demo

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,15 +1,23 @@
 package com.example;
 
+import com.example.utils.PerformanceInteceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @MapperScan("com.example.mapper")
-public class DemoApplication {
+public class DemoApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new PerformanceInteceptor())
+				.addPathPatterns("/user/**").addPathPatterns("/user/**");
+	}
 }
